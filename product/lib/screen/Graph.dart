5c62c2167cf2a54bcc0c4ac5a85e837cdc2df899
UesugiDaiki <fko2247021@stu.o-hara.ac.jp>
2024-11-14 11:40:47 +0900

    update: グラフタブでグラフ画面を表示

--- product/lib/screen/Graph.dart
+++ product/lib/screen/Graph.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:product/widgets/weekly_cal_bar_chart.dart';
+import 'package:product/widgets/monthly_cal_bar_chart.dart';
 
 class Graph extends StatefulWidget {
   const Graph({super.key});
@@ -20,75 +20,32 @@ class GraphState extends State<Graph> {
 
   @override
   Widget build(BuildContext context) {
-    // 列方向に指定
-    
-    final screenWidth = MediaQuery.of(context).size.width;
-    return 
-    SingleChildScrollView(
-      child: 
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      // 子要素
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            width: screenWidth * 0.95,
-            height: screenWidth * 0.95 * 0.65,
-            child:
-        // グラフ
-        BarChart(
-            BarChartData(
-              // 
-              borderData: FlBorderData(
-                border: const Border(
-                  top: BorderSide.none,
-                  right: BorderSide.none,
-                  left: BorderSide(width: 1),
-                  bottom: BorderSide(width: 1),
-                ),
-              ),
-              // タイトル
-              titlesData: const FlTitlesData(
-                topTitles: AxisTitles(
-                  axisNameWidget: Text(
-                    "グラフ",
-                  ),
-                  axisNameSize: 35.0,
-                ),
-                rightTitles:
-                    AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              ),
-              groupsSpace: 10,
-              barGroups: [
-                BarChartGroupData(x: 1, barRods: [
-                  BarChartRodData(toY: 1, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 2, barRods: [
-                  BarChartRodData(toY: 20, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 3, barRods: [
-                  BarChartRodData(toY: 30, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 4, barRods: [
-                  BarChartRodData(toY: 60, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 5, barRods: [
-                  BarChartRodData(toY: 90, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 6, barRods: [
-                  BarChartRodData(toY: 1000, width: 15, color: Colors.blue),
-                ]),
-                BarChartGroupData(x: 7, barRods: [
-                  BarChartRodData(toY: 90, width: 15, color: Colors.blue),
-                ]),
-              ],
-            ),
-          ),),
+    // 列方向に指定    
+    return const DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          TabBar(
+            tabs: [
+              Tab(text: "一週間"),
+              Tab(text: "一か月"),
+            ],
           ),
-        // 摂取した食品一覧
-      ]
-    ));
+          Expanded(
+            child: TabBarView(
+              children: [
+                SingleChildScrollView(
+                  child: WeeklyCalBarChart(title: "weekly calorie bar chart"),
+                ),
+                SingleChildScrollView(
+                  child: MonthlyCalBarChart(title: "monthly calorie bar chart")
+                )
+              ]
+            )
+          )
+        ],
+      ),
+    );
   }
 }
 // settings設定アイコン、search検索アイコン
