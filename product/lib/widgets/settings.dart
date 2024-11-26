e81d896811f259a49b07a251c0ab3c8f5a600270
amor0000 <fides5952.free@gmail.com>
2024-11-26 14:48:55 +0900

    update: 身体活動レベルと目標活動エネルギーを追加

--- product/lib/widgets/settings.dart
+++ product/lib/widgets/settings.dart
@@ -28,8 +28,11 @@ class SettingsState extends State<Settings> {
   final _Height = TextEditingController();
   final _Weight = TextEditingController();
   final _Age = TextEditingController();
+  final _Name = TextEditingController();
+  final _Target_kcal = TextEditingController();
   static const TextStyle optionStyle = TextStyle(fontSize: 25, fontWeight: FontWeight.bold);
   int _isSelectedValue = 1;
+  double _isSelectedDropDown = 1.5;
   // radio:初期値
   Gender? _category = Gender.male;
 
@@ -104,7 +107,7 @@ class SettingsState extends State<Settings> {
             ),
         ),
         // コンテンツ
-        body: Column(
+        body: SingleChildScrollView(child:Column(
           children: [
             Text('個人情報', style: optionStyle,),
             Form(
@@ -113,13 +116,40 @@ class SettingsState extends State<Settings> {
               Column(
                 children: [
                   // 入力欄
+                  textFormField(Icons.search,'名前', _Name, '名前', FilteringTextInputFormatter.singleLineFormatter, TextInputType.name, 10),
                   textFormField(Icons.search,'身長', _Height, '身長', FilteringTextInputFormatter.allow(RegExp(r'^\d+(\.\d*)?')), TextInputType.numberWithOptions(decimal: true)),
-                  textFormField(Icons.search,'体重', _Weight, '体重', FilteringTextInputFormatter.allow(RegExp(r'^\d+(\.\d*)?')), TextInputType.numberWithOptions(decimal: true), 4),
+                  textFormField(Icons.search,'体重', _Weight, '体重', FilteringTextInputFormatter.allow(RegExp(r'^\d+(\.\d*)?')), TextInputType.numberWithOptions(decimal: true), 5),
                   textFormField(Icons.search,'年齢', _Age, '年齢', 
                   FilteringTextInputFormatter.digitsOnly, TextInputType.number, 3),
+                  textFormField(Icons.search,'目標エネルギー', _Target_kcal, '目標エネルギー', FilteringTextInputFormatter.digitsOnly, TextInputType.number, 5),
                   // ラジオボタン
+                  Text('性別'),
                   listTitle(Gender.male),
                   listTitle(Gender.female),
+                  // ドロップダウン
+                  Text('身体活動レベル'),
+                  DropdownButton(
+                    items: const[
+                      DropdownMenuItem(
+                        value: 1.5,
+                        child: Text('低頻度運動'),
+                      ),
+                      DropdownMenuItem(
+                          value: 1.7,
+                          child: Text('中頻度の運動'),
+                      ),
+                      DropdownMenuItem(
+                          value: 1.9,
+                          child: Text('高頻度の運動'),
+                      ),
+                    ],
+                      value: _isSelectedDropDown,
+                      onChanged: (double? value) {
+                        setState(() {
+                          _isSelectedDropDown = value!;
+                        });
+                      }
+                    ),
                   Padding(
                   // 上下に16px余白を設定
                   padding: const EdgeInsets.symmetric(vertical: 16),
@@ -157,7 +187,7 @@ class SettingsState extends State<Settings> {
               )
             ),
           ],
-        )
+        ))
       );
   }
 }
